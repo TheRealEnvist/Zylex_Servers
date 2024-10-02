d6f03e51d470e5099ab9dc87f4fcb04b92e4b7f2
TheRealEnvist <137974409+TheRealEnvist@users.noreply.github.com>
2024-10-02 17:10:33 -0400

    Updated Server

--- Zylex_Servers/Program.cs
+++ Zylex_Servers/Program.cs
@@ -2,10 +2,21 @@
 {
     internal class Program
     {
+        public byte ServerType;
+        public byte GameEngineType;
         static void Main(string[] args)
         {
-            Console.WriteLine("Starting servers.....");
-            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("--Server Installer--");
+            Console.WriteLine("");
+            Console.WriteLine("What kind of server would you like?");
+            Console.WriteLine("");
+            Console.WriteLine("1. Game Server");
+            Console.WriteLine("2. Generic Server");
+            Console.WriteLine("");
+            Console.Write("Server Type: ");
+            string type = Console.ReadLine();
+
         }
     }
 }
